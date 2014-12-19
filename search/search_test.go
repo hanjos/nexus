b807756ee9895d8d1538839b5c3ffc0790387663
Humberto Anjos <h.anjos@acm.org>
2014-12-19 14:43:36 -0200

    More tests for search.

--- search/search_test.go
+++ search/search_test.go
@@ -8,9 +8,32 @@ import (
 	"testing"
 )
 
+func checkMap(t *testing.T, expected map[string]string, actual map[string]string) {
+	if len(expected) != len(actual) {
+		t.Errorf("Wrong number of fields: expected %v, got %v", len(expected), len(actual))
+	}
+
+	for k, v := range actual {
+		vExp, ok := expected[k]
+
+		if !ok {
+			t.Errorf("Unexpected field %q", k)
+		} else if vExp != v {
+			t.Errorf("Expected value %q for field %q, got %q", vExp, k, v)
+		}
+	}
+}
+
 func TestAllImplementsCriteria(t *testing.T) {
 	if _, ok := interface{}(search.All).(search.Criteria); !ok {
 		t.Errorf("search.All does not implement Criteria!")
+	}
+}
+
+func TestAllProvidesNoCriteria(t *testing.T) {
+	criteria := search.All.Parameters()
+	if len(criteria) != 0 {
+		t.Errorf("expected an empty map, got %v", criteria)
 	}
 }
 
@@ -20,10 +43,57 @@ func TestByCoordinatesImplementsCriteria(t *testing.T) {
 	}
 }
 
+type pair struct {
+	expected string
+	actual   string
+}
+
+func TestByCoordinatesSetsTheProperFields(t *testing.T) {
+	criteria := search.ByCoordinates{GroupId: "g", ArtifactId: "a", Version: "v", Packaging: "p", Classifier: "c"}.Parameters()
+
+	expected := []string{"g", "a", "v", "p", "c"}
+	missing := []string{}
+	wrong := []pair{}
+
+	for _, exp := range expected {
+		v, ok := criteria[exp]
+
+		if !ok {
+			missing = append(missing, exp)
+		}
+		if v != exp {
+			wrong = append(wrong, pair{exp, v})
+		}
+	}
+
+	if len(missing) != 0 {
+		t.Errorf("Missing fields %v", missing)
+	}
+
+	if len(wrong) != 0 {
+		t.Errorf("Fields with wrong values:\n")
+		for _, p := range wrong {
+			t.Errorf("Field %q expected value %q, got %q", p.expected, p.expected, p.actual)
+		}
+	}
+}
+
+func TestByCoordinatesSetsOnlyTheGivenFields(t *testing.T) {
+	criteria := search.ByCoordinates{GroupId: "g", ArtifactId: "a"}.Parameters()
+
+	checkMap(t, map[string]string{"g": "g", "a": "a"}, criteria)
+}
+
 func TestByClassnameImplementsCriteria(t *testing.T) {
 	if _, ok := interface{}(search.ByClassname("")).(search.Criteria); !ok {
 		t.Errorf("search.ByClassname does not implement Criteria!")
 	}
+}
+
+func TestByClassnameSetsTheProperFields(t *testing.T) {
+	criteria := search.ByClassname("cn").Parameters()
+
+	checkMap(t, map[string]string{"cn": "cn"}, criteria)
 }
 
 func TestByChecksumImplementsCriteria(t *testing.T) {
@@ -32,10 +102,22 @@ func TestByChecksumImplementsCriteria(t *testing.T) {
 	}
 }
 
+func TestByChecksumSetsTheProperFields(t *testing.T) {
+	criteria := search.ByChecksum("sha1").Parameters()
+
+	checkMap(t, map[string]string{"sha1": "sha1"}, criteria)
+}
+
 func TestByKeywordImplementsCriteria(t *testing.T) {
 	if _, ok := interface{}(search.ByKeyword("")).(search.Criteria); !ok {
 		t.Errorf("search.ByKeyword does not implement Criteria!")
 	}
+}
+
+func TestByKeywordSetsTheProperFields(t *testing.T) {
+	criteria := search.ByKeyword("q").Parameters()
+
+	checkMap(t, map[string]string{"q": "q"}, criteria)
 }
 
 func TestByRepositoryImplementsCriteria(t *testing.T) {
@@ -44,10 +126,22 @@ func TestByRepositoryImplementsCriteria(t *testing.T) {
 	}
 }
 
+func TestByRepositorySetsTheProperFields(t *testing.T) {
+	criteria := search.ByRepository("repositoryId").Parameters()
+
+	checkMap(t, map[string]string{"repositoryId": "repositoryId"}, criteria)
+}
+
 func TestInRepositoryImplementsCriteria(t *testing.T) {
 	if _, ok := interface{}(search.InRepository{}).(search.Criteria); !ok {
 		t.Errorf("search.InRepository does not implement Criteria!")
 	}
+}
+
+func TestInRepositorySetsTheProperFields(t *testing.T) {
+	criteria := search.InRepository{"repositoryId", search.ByChecksum("sha1")}.Parameters()
+
+	checkMap(t, map[string]string{"repositoryId": "repositoryId", "sha1": "sha1"}, criteria)
 }
 
 // Examples
