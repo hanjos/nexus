3cf2de9601998e63c4203db49204f994e38a8c93
Humberto Anjos <h.anjos@acm.org>
2014-12-19 10:53:33 -0200

    Tests for search.

--- search/search_test.go
+++ search/search_test.go
@@ -4,18 +4,64 @@ import (
 	"github.com/hanjos/nexus"
 	"github.com/hanjos/nexus/credentials"
 	"github.com/hanjos/nexus/search"
+
+	"testing"
 )
+
+func TestAllImplementsCriteria(t *testing.T) {
+	if _, ok := interface{}(search.All).(search.Criteria); !ok {
+		t.Errorf("search.All does not implement Criteria!")
+	}
+}
+
+func TestByCoordinatesImplementsCriteria(t *testing.T) {
+	if _, ok := interface{}(search.ByCoordinates{}).(search.Criteria); !ok {
+		t.Errorf("search.ByCoordinates does not implement Criteria!")
+	}
+}
+
+func TestByClassnameImplementsCriteria(t *testing.T) {
+	if _, ok := interface{}(search.ByClassname("")).(search.Criteria); !ok {
+		t.Errorf("search.ByClassname does not implement Criteria!")
+	}
+}
+
+func TestByChecksumImplementsCriteria(t *testing.T) {
+	if _, ok := interface{}(search.ByChecksum("")).(search.Criteria); !ok {
+		t.Errorf("search.ByChecksum does not implement Criteria!")
+	}
+}
+
+func TestByKeywordImplementsCriteria(t *testing.T) {
+	if _, ok := interface{}(search.ByKeyword("")).(search.Criteria); !ok {
+		t.Errorf("search.ByKeyword does not implement Criteria!")
+	}
+}
+
+func TestByRepositoryImplementsCriteria(t *testing.T) {
+	if _, ok := interface{}(search.ByRepository("")).(search.Criteria); !ok {
+		t.Errorf("search.ByRepository does not implement Criteria!")
+	}
+}
+
+func TestInRepositoryImplementsCriteria(t *testing.T) {
+	if _, ok := interface{}(search.InRepository{}).(search.Criteria); !ok {
+		t.Errorf("search.InRepository does not implement Criteria!")
+	}
+}
+
+// Examples
 
 func ExampleByKeyword() {
 	n := nexus.New("https://maven.java.net", credentials.None)
 
 	// Return all artifacts with javax.enterprise somewhere.
-	artifacts, err := n.Artifacts(search.ByKeyword("javax.enterprise*"))
+	n.Artifacts(search.ByKeyword("javax.enterprise*"))
 
 	// This search may or may not return an error, depending on the version of
 	// the Nexus being accessed. On newer Nexuses (sp?) "*" searches are
 	// invalid.
-	artifacts, err := n.Artifacts(search.ByKeyword("*"))
+	n.Artifacts(search.ByKeyword("*"))
 }
 
 func ExampleByCoordinates() {
@@ -24,26 +70,24 @@ func ExampleByCoordinates() {
 	// Returns all artifacts with a groupId starting with com.sun. Due to Go's
 	// struct syntax, we don't need to specify all the coordinates; they
 	// default to string's zero value (""), which Nexus ignores.
-	artifacts, _ := n.Artifacts(search.ByCoordinates{GroupId: "com.sun*"})
+	n.Artifacts(search.ByCoordinates{GroupId: "com.sun*"})
 
 	// A coordinate search requires specifying at least either a groupId, an
 	// artifactId or a version. This search will (after some time), return
 	// nothing. This doesn't mean there are no projects with packaging "pom";
 	// this is a limitation of Nexus' search.
-	artifacts, _ := n.Artifacts(search.ByCoordinates{Packaging: "pom"})
+	n.Artifacts(search.ByCoordinates{Packaging: "pom"})
 
 	// This search may or may not return an error, depending on the version of
 	// the Nexus being accessed. On newer Nexuses (sp?) "*" searches are
 	// invalid.
-	artifacts, err := n.Artifacts(
-		search.ByCoordinates{GroupId: "*", Packaging: "pom"})
+	n.Artifacts(search.ByCoordinates{GroupId: "*", Packaging: "pom"})
 
 	// ByCoordinates searches in Maven *projects*, not artifacts. So this
 	// search will return all com.sun* artifacts in projects with packaging
 	// "pom", not all POM artifacts with groupId com.sun*! Packaging is not
 	// the same as extension.
-	artifacts, _ := n.Artifacts(
-		search.ByCoordinates{GroupId: "com*", Packaging: "pom"})
+	n.Artifacts(search.ByCoordinates{GroupId: "com*", Packaging: "pom"})
 }
 
 func ExampleInRepository() {
@@ -51,7 +95,7 @@ func ExampleInRepository() {
 
 	// Returns all artifacts in the repository releases with groupId starting
 	// with com.sun and whose project has packaging "pom".
-	artifacts, _ := n.Artifacts(
+	n.Artifacts(
 		search.InRepository{
 			"releases",
 			search.ByCoordinates{GroupId: "com.sun*", Packaging: "pom"},
@@ -59,7 +103,7 @@ func ExampleInRepository() {
 
 	// Nexus doesn't support * in the repository ID parameter, so this search
 	// will return an error.
-	_, err := n.Artifacts(
+	n.Artifacts(
 		search.InRepository{
 			"releases*",
 			search.ByCoordinates{GroupId: "com.sun*", Packaging: "pom"},
