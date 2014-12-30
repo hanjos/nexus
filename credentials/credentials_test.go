3d483744df9c28341166b1b2032698a55314e8e2
Humberto Anjos <h.anjos@acm.org>
2014-12-30 10:04:48 -0200

    Some tests for credentials.

--- credentials/credentials_test.go
+++ credentials/credentials_test.go
@@ -5,6 +5,31 @@ import (
 	"testing"
 )
 
+func TestNoneImplementsCredentials(t *testing.T) {
+	if _, ok := interface{}(credentials.None).(credentials.Credentials); !ok {
+		t.Errorf("credentials.None doesn't implement credentials.Credentials!")
+	}
+}
+
+func TestBasicAuthImplementsCredentials(t *testing.T) {
+	if _, ok := interface{}(credentials.BasicAuth{"", ""}).(credentials.Credentials); !ok {
+		t.Errorf("credentials.None doesn't implement credentials.Credentials!")
+	}
+}
+
+func TestOrZeroReturnsTheGivenNonNilArgument(t *testing.T) {
+	c := credentials.BasicAuth{"", ""}
+	if v := credentials.OrZero(c); v != c {
+		t.Errorf("credentials.OrZero(%v) should've returned %v, not %v!", c, c, v)
+	}
+}
+
+func TestOrZeroReturnsNoneOnNil(t *testing.T) {
+	if v := credentials.OrZero(nil); v != credentials.None {
+		t.Errorf("credentials.OrZero(nil) should've returned credentials.None, not %v!", v)
+	}
+}
+
 func TestNoneSignDoesntBarfOnNil(t *testing.T) {
 	defer func() {
 		if r := recover(); r != nil {
